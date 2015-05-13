b14f68e1002ae2788a3e9a413e7c38343e2ff43d
Vadim Markovtsev <v.markovtsev@samsung.com>
2015-05-13 19:19:32 +0300

    Added the slide on label stats

---
+++ src/js/offset_log_annotations.js
@@ -0,0 +1,12 @@
+var parent_height = $("#loader-sample").innerHeight();
+var line_height = $("#loader-sample > span").height();
+var la = $(".log-annotation");
+var la_height = la.height();
+
+la.each(function() {
+    var top = $(this).next().position().top;
+    var new_top = top - (la_height - line_height) / 2;
+    var new_top_pt = new_top / parent_height * 100;
+    $(this).css({top: new_top_pt + "%"});
+    console.log(new_top_pt);
+});
